be1ef52e4f6ab022194277dcabcafe76f0ed74f3
immortalcodes <21112002mj@gmail.com>
2022-07-06 11:42:18 +0530

    Some basic customizations

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -2,12 +2,13 @@
 import { Fragment } from 'react'
 import { Disclosure, Menu, Transition } from '@headlessui/react'
 import { BellIcon, MenuIcon, XIcon } from '@heroicons/react/outline'
-
+import profile_image from '../assets/img/profile.png'
 const navigation = [
   { name: 'Dashboard', href: '#', current: true },
-  { name: 'Team', href: '#', current: false },
-  { name: 'Projects', href: '#', current: false },
-  { name: 'Calendar', href: '#', current: false },
+  { name: 'Products', href: '#', current: false },
+  { name: 'Settings', href: '#', current: false },
+  { name: 'About', href: '#', current: false }
+
 ]
 
 function classNames(...classes) {
@@ -19,7 +20,7 @@ export default function Navbar() {
     <Disclosure as="nav" className="bg-gray-800">
       {({ open }) => (
         <>
-          <div className="max-w-7xl mr-auto px-2 sm:px-6 lg:px-8">
+          <div className="max-w-7x2 px-2 sm:px-6 lg:px-8">
             <div className="relative flex items-center justify-between h-16">
               <div className="absolute inset-y-0 left-0 flex items-center sm:hidden">
                 {/* Mobile menu button*/}
@@ -34,7 +35,7 @@ export default function Navbar() {
               </div>
               <div className="flex-1 flex items-center justify-center sm:items-stretch sm:justify-start">
                 <div className="flex-shrink-0 flex items-center">
-                <div className='font-sans font-black text-amber-300 text-left	'>MeriDukkan</div>
+                <div className='font-sans font-black text-amber-300 text-left'>MeriDukkan</div>
                 </div>
                 <div className="hidden sm:block sm:ml-6">
                   <div className="flex space-x-4">
@@ -70,7 +71,7 @@ export default function Navbar() {
                       <span className="sr-only">Open user menu</span>
                       <img
                         className="h-8 w-8 rounded-full"
-                        src="https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80"
+                        src= {profile_image}
                         alt=""
                       />
                     </Menu.Button>
@@ -91,17 +92,7 @@ export default function Navbar() {
                             href="#"
                             className={classNames(active ? 'bg-gray-100' : '', 'block px-4 py-2 text-sm text-gray-700')}
                           >
-                            Your Profile
-                          </a>
-                        )}
-                      </Menu.Item>
-                      <Menu.Item>
-                        {({ active }) => (
-                          <a
-                            href="#"
-                            className={classNames(active ? 'bg-gray-100' : '', 'block px-4 py-2 text-sm text-gray-700')}
-                          >
-                            Settings
+                            My Profile
                           </a>
                         )}
                       </Menu.Item>
