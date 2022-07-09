6342b6c10fb4ebb58af3c1a6497e328f242a7f03
immortalcodes <21112002mj@gmail.com>
2022-07-09 17:48:47 +0530

    added footer

--- src/index.js
+++ src/index.js
@@ -4,11 +4,13 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import Navbar from './components/Navbar';
+import Footer from './components/Footer';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <Navbar />
+    <Footer/>
   </React.StrictMode>
 );
 
