d2fa9866c7483d8f7ae6b6ccc4d32fc635bda6a7
immortalcodes <21112002mj@gmail.com>
2022-07-22 22:20:02 +0530

    changes to navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -42,6 +42,7 @@ export default function Navbar() {
                     {navigation.map((item) => (
                       <a
                         key={item.name}
+                        id={"navite" + item.name}
                         href={item.href}
                         className={classNames(
                           item.current ? 'bg-gray-900 text-white' : 'text-gray-300 hover:bg-gray-700 hover:text-white',
@@ -51,6 +52,7 @@ export default function Navbar() {
                       >
                         {item.name}
                       </a>
+                      
                     ))}
                   </div>
                 </div>
